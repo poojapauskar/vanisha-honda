0c889f0b27af93947dd02cc19a35bec9a15a7bbc
poojapauskar <git.poojapauskar@gmail.com>
2016-10-19 11:26:49 +0530

    customer database added

---
+++ customer_database.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Vanisha Honda</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <script src="bootstrap/js/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+
+
+<!-- datepicker -->
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-animate.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-aria.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular-material/1.0.4/angular-material.js"></script>
+<script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-114/assets-cache.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/angular-material/1.0.4/angular-material.css">
+<script src="js/datepicker.js"></script>   
+
+
+  <script src="js/material.min.js"></script>
+  <link rel="stylesheet" href="css/material.indigo-pink.min.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+</head>
+<body ng-app="" style="overflow-x:hidden">
+
+<div class="row">
+  <div class="col-sm-4">
+  <a href="admin_panel.php">
+    <img style="width:13%;height:13%;margin-top:4%;margin-left:4%" src="images/sample_logo.jpg"></img>
+    <h6 style="color:black;margin-top:-5%;margin-left:19%">Vanisha Honda</h6>
+    <img style="width:9%;height:9%;margin-top:-23%;margin-left:48%" src="images/home.png"></img>
+  </a>
+  </div>
+  <div class="col-sm-4" style="text-align:center">
+    <h5>Customer Database</h5>
+  </div>
+  <div class="col-sm-4">
+  
+ 
+          <!-- Navigation -->
+          <nav class="mdl-navigation" style="margin-top:4%">
+            <a class="mdl-navigation__link" href=""><img style="width:13%;height:13%;" src="images/bell.png"></img>Welcome User,</a>
+            <a style="color:red" class="mdl-navigation__link" href="#">Logout</a>
+          </nav>
+        </div>
+      
+  </div>
+</div>
+  
+<div class="container">
+  <div class="row" style="margin-top:5%">
+    <div class="col-sm-2">
+    </div>
+    <div class="col-sm-2">
+      <h6 style="margin-top:-8%;font-weight:bold">Recent Search</h6>
+      <form action="#" style="margin-top:-20%">
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+          <label class="mdl-button mdl-js-button mdl-button--icon" for="sample6">
+            <i class="material-icons">search</i>
+          </label>
+          <div class="mdl-textfield__expandable-holder">
+            <input class="mdl-textfield__input" type="text" id="sample6">
+            <label class="mdl-textfield__label" for="sample-expandable">Expandable Input</label>
+          </div>
+        </div>
+      </form>
+    </div>
+
+
+    <div class="col-sm-5">
+
+<html ng-app="datepickerBasicUsage">
+<div ng-controller="AppCtrl" style=''>
+    <md-content>
+      From <md-datepicker ng-model="myDate1" md-placeholder="Enter date"></md-datepicker>
+      To <md-datepicker ng-model="myDate2" md-placeholder="Enter date"></md-datepicker>
+     <!--  <h4>Date-picker with min date and max date</h4>
+      <md-datepicker ng-model="myDate" placeholder="Enter date"
+                 md-min-date="minDate" md-max-date="maxDate"></md-datepicker> -->
+    </md-content>
+  </div>
+
+
+
+    </div>
+    <div class="col-sm-1">
+      <button class="mdl-button mdl-js-button mdl-button--raised">
+        Search
+      </button>
+    </div>
+    <div class="col-sm-1">
+      <button class="mdl-button mdl-js-button mdl-button--raised">
+        Export/Print 
+      </button>
+    </div>
+    <!-- <div class="col-sm-1">
+      <button class="mdl-button mdl-js-button mdl-button--raised">
+        Add New
+      </button>
+    </div> -->
+  </div>
+</div>
+
+    <!-- Textfield with Floating Label -->
+
+<div class="row">
+<table  align="center" class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+  <thead>
+    <tr>
+      <th class="mdl-data-table__cell--non-numeric">Name</th>
+      <th>Vehicle</th>
+      <th>Vehicle Co</th>
+      <th>Engine No.</th>
+      <th>Chassis No.</th>
+      <th>Reg. No.</th>
+      <th>Mobile</th>
+      <th>Email</th>
+      <th>Address</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Pleasure</td>
+      <td>Mathew</td>
+      <td>jason@bitjini.com</td>
+      <td>Pleasure</td>
+      <td>hjkhk22333</td>
+      <td>87979hj</td>
+      <td>Rs.2000</td>
+      <td>12/4/2014</td>
+      <td>12/4/2014</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Jason Matt</td>
+      <td>9123456789</td>
+      <td>jason@bitjini.com</td>
+      <td>Pleasure</td>
+      <td>hjkhk22333</td>
+      <td>87979hj</td>
+      <td>Rs.2000</td>
+      <td>12/4/2014</td>
+      <td>12/4/2014</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Jason Matt</td>
+      <td>9123456789</td>
+      <td>jason@bitjini.com</td>
+      <td>Pleasure</td>
+      <td>hjkhk22333</td>
+      <td>87979hj</td>
+      <td>Rs.2000</td>
+      <td>12/4/2014</td>
+      <td>12/4/2014</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Jason Matt</td>
+      <td>9123456789</td>
+      <td>jason@bitjini.com</td>
+      <td>Pleasure</td>
+      <td>hjkhk22333</td>
+      <td>87979hj</td>
+      <td>Rs.2000</td>
+      <td>12/4/2014</td>
+      <td>12/4/2014</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Jason Matt</td>
+      <td>9123456789</td>
+      <td>jason@bitjini.com</td>
+      <td>Pleasure</td>
+      <td>hjkhk22333</td>
+      <td>87979hj</td>
+      <td>Rs.2000</td>
+      <td>12/4/2014</td>
+      <td>12/4/2014</td>
+    </tr>
+  </tbody>
+</table>
+</div>  
+    
+
+</body>
+</html>
+
