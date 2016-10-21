9a9f5c5558ac32949bf917d84b77573af02a717d
poojapauskar <git.poojapauskar@gmail.com>
2016-10-21 13:23:24 +0530

    sliders added

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
     <!-- Material Design icon font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
-
   <link rel="stylesheet" href="css/style.css">
   <script src="js/index.js"></script>
 </head>
@@ -99,6 +98,76 @@
 </nav>
   
 <div class="container">
+
+<div style="text-align:center !important" class="row">
+
+<div id="slider1" class="csslider infinity">
+  <input type="radio" name="slides" id="slides_1">
+  <input type="radio" name="slides" id="slides_2">
+  <input type="radio" name="slides" checked="checked" id="slides_3">
+  <input type="radio" name="slides" id="slides_4">
+  <input type="radio" name="slides" id="slides_5">
+  <ul>
+    <li><h2>Slide 1</h2></li>
+    <li><h2>Slide 2</h2></li>
+    <li>
+          <div class="row">
+              <div class="col-sm-6">
+                <h5>Introducing the new</h5>
+                <h4 style="margin-top:-5%">Vanisha Honda Android App</h5>
+                <a style="margin-top:-5%" href="#">Vehicle Booking</a><br>
+                <a style="margin-top:-5%" href="#">Book test ride</a>
+                        <h6 >Download Now</h6>
+                        <img style="width:50%;height:50%" src="images/google-play-badge.png"></img>
+              </div>
+              <div class="col-sm-6">
+              <img style="width:30%;height:30%" src="images/mobile.png"></img>
+              </div>
+            </div>
+    </li>
+    <li><h2>Slide 4</h2></li>
+    <li><h2>Slide 5</h2></li>
+    <li><h2>Slide 6</h2></li>
+  </ul>
+  <div class="arrows">
+    <label for="slides_1"></label>
+    <label for="slides_2"></label>
+    <label for="slides_3"></label>
+    <label for="slides_4"></label>
+    <label for="slides_5"></label>
+    <label for="slides_1" class="goto-first"></label>
+    <label for="slides_5" class="goto-last"></label>
+  </div>
+  <div class="navigation">
+    <div>
+      <label for="slides_1"></label>
+      <label for="slides_2"></label>
+      <label for="slides_3"></label>
+      <label for="slides_4"></label>
+      <label for="slides_5"></label>
+    </div>
+  </div>
+</div>
+
+</div>
+
+<div class="row">
+<h5>About Us</h5>
+<p>In publishing and graphic design, lorem ipsum (derived from Latin dolorem ipsum, translated as "pain itself") is a filler text commonly used to demonstrate the graphic elements of a document or visual presentation. Replacing meaningful content with placeholder text allows designers to design the form of the content before the content itself has been produced.</p>
+</div>
+
+<div class="row">
+</div>
+
+<div class="row">
+</div>
+
+<div class="row">
+</div>
+
+<div class="row">
+</div>
+
 </div>
 
 </body>
