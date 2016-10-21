a3aef185a0f725d027f6d367e1449da82d92220a
poojapauskar <git.poojapauskar@gmail.com>
2016-10-21 17:42:30 +0530

    worked on finance page

---
+++ finance.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+  <link rel="stylesheet" href="css/slideshow.css">
+
+ <script src="https://code.getmdl.io/1.2.1/material.min.js"></script>
+    <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css">
+    <!-- Material Design icon font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/index.js"></script>
+</head>
+<body>
+
+
+<!-- datepicker -->
+<link rel="stylesheet" href="css/jquery-ui.css">
+<script src="js/jquery-ui.js"></script>
+  <script>
+  $(function() {
+    $( ".date" ).datepicker({ dateFormat: 'dd/mm/yy' });
+  });
+  </script>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <img style="width:8%;height:8%" src="images/sample_logo.jpg"></img>
+      <a class="" href="#">Vanisha Honda</a>
+    </div>
+
+<ul style="margin-top:-5%;margin-left:50%" class="nav navbar-nav">
+ <li class="active"><a href="#">Home</a></li>
+
+<li>
+<div class="dropdown" style="margin-left:10%">
+  <a href="#" class="btn dropdown-toggle" data-toggle="dropdown">Products<span class="caret"></span></a>
+  <ul class="dropdown-menu">
+    <li>
+      <a class="trigger right-caret">Level 1</a>
+      <ul class="dropdown-menu sub-menu">
+        <li><a href="#">Level 2</a></li>
+        <li>
+          <a class="trigger right-caret">Level 2</a>
+          <ul class="dropdown-menu sub-menu">
+            <li><a href="#">Level 3</a></li>
+            <li><a href="#">Level 3</a></li>
+            <li>
+              <a class="trigger right-caret">Level 3</a>
+              <ul class="dropdown-menu sub-menu">
+                <li><a href="#">Level 4</a></li>
+                <li><a href="#">Level 4</a></li>
+                <li><a href="#">Level 4</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li><a href="#">Level 2</a></li>
+      </ul>
+    </li>
+    <li><a href="#">Level 1</a></li>
+    <li><a href="#">Level 1</a></li>
+  </ul>
+</div>
+</li>
+
+<li>
+<div class="dropdown" style="margin-left:20%">
+  <a href="#" class="btn dropdown-toggle" data-toggle="dropdown">Customer<span class="caret"></span></a>
+  <ul class="dropdown-menu">
+    <li>
+      <a class="trigger right-caret">Level 1</a>
+      <ul class="dropdown-menu sub-menu">
+        <li><a href="#">Level 2</a></li>
+        <li>
+          <a class="trigger right-caret">Level 2</a>
+          <ul class="dropdown-menu sub-menu">
+            <li><a href="#">Level 3</a></li>
+            <li><a href="#">Level 3</a></li>
+            <li>
+              <a class="trigger right-caret">Level 3</a>
+              <ul class="dropdown-menu sub-menu">
+                <li><a href="#">Level 4</a></li>
+                <li><a href="#">Level 4</a></li>
+                <li><a href="#">Level 4</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li><a href="#">Level 2</a></li>
+      </ul>
+    </li>
+    <li><a href="#">Level 1</a></li>
+    <li><a href="#">Level 1</a></li>
+  </ul>
+</div>
+</li>
+
+      <li><a style="margin-left:14%" href="#">Contact</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+<h5 style="margin-top:-1%;color:red;text-align:center">Get Finance</h5>
+
+<div class="row">
+  <div class="col-sm-6">
+    <img style="width:150px;height:150px" src="images/book_service.png"></img>
+  </div>
+
+  <div class="col-sm-6">
+      <form action="#">
+      
+          <div class="demo">
+            <!-- Standard Select -->
+            <div class="mdl-selectfield">
+              <label>Select Vehicle</label>
+              <select class="browser-default">
+                <option value="" disabled selected>Model</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+              </select>
+            </div>
+          </div>
+
+          <div style="align:left;margin-top:-2%" class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" id="sample1">
+            <label class="mdl-textfield__label" for="sample1">Price</label>
+          </div>
+
+          <div style="align:left;margin-top:-2%" class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" id="sample1">
+            <label class="mdl-textfield__label" for="sample1">Down Payment</label>
+          </div>
+<br>
+          ID Proof: <input type="file" id="myFile1">
+          Add. Proof: <input type="file" id="myFile2">
+
+          <div style="align:left;margin-top:-2%" class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" id="sample1">
+            <label class="mdl-textfield__label" for="sample1">PAN</label>
+          </div>
+<br>
+          6 Months Bank Statement: <input type="file" id="myFile3">
+          Salary Slip: <input type="file" id="myFile4">
+          IT Returns: <input type="file" id="myFile5">
+
+          <br>
+          
+          <button style="background-color:red;color:white" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+            Get Approval
+          </button>
+      </form>
+  </div>
+</div>
+
+<div style="height:100px" class="row">
+</div>
+
+<div style="background-color:red" class="row">
+  <div class="col-sm-6" style="color:white;">
+       <div style="margin-left:10%">
+        <img style="width:8%;height:8%" src="images/sample_logo.jpg"></img>
+        <h5 style="margin-top:-5%;margin-left:10%">Vanisha Honda</h5>
+        <h6 style="font-size:12px">9123456789, 97181717817<br>
+        9123456789, 97181717817<br>
+        info@vanishahonda.com</h6>
+
+        <form>
+        <input type="text" placeholder="Enter email" id="email1" name="email1"></input>
+        <button style="color:white;background-color:blue" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+          Submit
+        </button>
+        </form>
+
+       </div>
+  </div>
+  <div class="col-sm-6" style="color:white">
+       <div style="margin-left:10%">
+       <h6 style="font-size:12px;margin-top:10%">9123456789, 97181717817<br>
+        9123456789, 97181717817<br>
+        info@vanishahonda.com</h6>
+       </div>
+  </div>
+</div>
+
+
+<div style="background-color:red" class="row">
+<h4 style="font-size:12px;color:white;text-align:center">2016, Vanisha Honda. All rights reserver</h4>
+</div>
+
+</body>
+</html>
+
+
+
+
+</body>
+</html>
+
